faf9fb74d3f69b4d2ef6975d365db406d51748af
Kevin Morris <kevin.d.morris4@gmail.com>
2025-08-01 13:06:16 -0400

    Send messages to all clients

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
   ws.on("message", (msg) => {
-    ws.send(msg.toString());
+    wss.clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) {
+        client.send(msg.toString());
+      }
+    });
   });
 });
 
